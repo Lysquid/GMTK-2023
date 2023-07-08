60044009d2885bdd6fd22d2935dcd3aaad84e649
ALecorche <adriaan.lecorche@insa-lyon.fr>
2023-07-08 11:48:37 +0200

    zombie colision and death

--- src/entities/zombie.gd
+++ src/entities/zombie.gd
@@ -3,9 +3,11 @@ extends CharacterBody2D
 const SPEED: float = 40
 const ARROW_DIST: float = 20
 
-var direction: Vector2 = Vector2(1, 0)
 var mouse_on_zombie: bool = false
 var selected: bool = false
+var direction: Vector2 = Vector2(1, 0)
+
+var alive: bool = true
 
 
 func _ready():
@@ -14,28 +16,33 @@ func _ready():
 
 func get_dir():
 	var mouse_pos = get_viewport().get_mouse_position()
-	
 	return (mouse_pos - position).normalized()
 
 
 func _process(delta):
+	
 	$Arrow.visible = selected
 	
+	if !alive: return
+	
+	var click = Input.is_action_just_pressed("select")
+	var dir = get_dir()
+	
+	# updating selected variable
 	if selected:
-		var dir = get_dir()
+		if click:
+			selected = false
+			direction = dir
+	else:
+		if click and mouse_on_zombie:
+			selected = true
+	
+	# applying selection
+	if selected:
 		$Arrow.position = dir * ARROW_DIST
 		var angle = $Arrow.get_angle_to(position + dir * 2 * ARROW_DIST)
 		$Arrow.rotate(angle)
-		
-		if Input.is_action_just_pressed("select"):
-			selected = false
-			direction = dir
-			print(direction)
 	else:
-		if Input.is_action_just_pressed("select") and mouse_on_zombie:
-			selected = true
-	
-	if !selected:
 		move_and_collide(direction * SPEED * delta)
 
 
@@ -45,3 +52,12 @@ func _on_area_2d_mouse_entered():
 
 func _on_area_2d_mouse_exited():
 	mouse_on_zombie = false
+
+
+func _on_area_2d_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body == self or !self.alive or !body.is_in_group("zombies"): return
+	
+	self.alive = false
+	self.selected = false
+	body.alive = false
+	body.selected = false
