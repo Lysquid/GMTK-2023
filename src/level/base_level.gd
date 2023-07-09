6fccffe48e7124ac9c667c2e0818a2d40bb2a9a5
Lysquid <romainbenoit21560@gmail.com>
2023-07-09 14:55:40 +0200

    hud

--- src/level/base_level.gd
+++ src/level/base_level.gd
@@ -5,14 +5,20 @@ class_name Level
 signal level_complete(surviving_zombies)
 signal game_over
 
+var end = false
+
 func _process(delta):
 	
-	var humans := get_tree().get_nodes_in_group('humans')
-	var zombies := get_tree().get_nodes_in_group('zombies')
+	if not end:
 	
-	if humans.is_empty():
-		level_complete.emit(zombies.size())
-	
-	if zombies.is_empty():
-		game_over.emit()
+		var humans := get_tree().get_nodes_in_group('humans')
+		var zombies := get_tree().get_nodes_in_group('zombies')
+		
+		if humans.is_empty():
+			level_complete.emit(zombies.size())
+			end = true
+		
+		if zombies.is_empty():
+			game_over.emit()
+			end = true
 	
