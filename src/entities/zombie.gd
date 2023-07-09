a751c143dbe2c02f03b6b4f0135b584790db8f32
ALecorche <adriaan.lecorche@insa-lyon.fr>
2023-07-09 11:28:42 +0200

    added pitch shift

--- src/entities/zombie.gd
+++ src/entities/zombie.gd
@@ -35,7 +35,7 @@ func unselect():
 	is_idle = false
 	Engine.time_scale = 1
 	var main = get_node("/root/Main")
-	main.$AudioStreamPlayer2D.pitch_scale = 1
+	main.get_node("AudioStreamPlayer2D").pitch_scale = 1
 
 func can_see(target: Vector2):
 	var space_state = get_world_2d().direct_space_state
@@ -71,7 +71,7 @@ func _process(delta):
 			get_parent().select()
 			Engine.time_scale = SLOW_TIME_SCALE
 			var main = get_node("/root/Main")
-			main.$AudioStreamPlayer2D.pitch_scale = 0.5
+			main.get_node("AudioStreamPlayer2D").pitch_scale = 0.5
 	
 	# showing selection
 	if selected:
